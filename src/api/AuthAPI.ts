6ae0681a6870cad17a78379bcb5038678b85c09c
m0000amir <mukhtarov.amir.a@gmail.com>
2023-01-22 01:43:48 +0300

    check husky

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
+
 import BaseAPI from './BaseAPI';
 import { SigninData, SignupData, User } from '../types/types';
-import { Options } from '../utils/HTTPTransport';
 
 export class AuthAPI extends BaseAPI {
   constructor() {
@@ -15,7 +15,7 @@ export class AuthAPI extends BaseAPI {
     return this.http.post('/signup', { data: signupData });
   }
 
-  publicasd read(): Promise<User> { // getUser
+  public read(): Promise<User> { // getUser
     return this.http.get('/user');
   }
 
